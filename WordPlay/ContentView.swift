2a5dcdf85aa89cbbd17f6498619093bd1978b86f
Ufuoma Okoro <ufookoro@gmail.com>
2020-03-28 12:36:13 +0000

    Initial Commit

---
+++ WordPlay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordPlay
+//
+//  Created by Ufuoma Okoro on 28/03/2020.
+//  Copyright © 2020 Ufuoma Okoro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
